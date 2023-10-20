17e823a056c2f93c65f8375b664eb0657c4eab0e
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 21:26:03 +0200

    NestJS Auth and Authorization

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,5 +1,6 @@
 import {
   ConflictException,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from "@nestjs/common";
@@ -9,12 +10,18 @@ import { Repository } from "typeorm";
 import { HashingService } from "../hashing/hashing.service";
 import { SignUpDto } from "./dto/sign-up.dto";
 import { SignInDto } from "./dto/sign-in.dto";
+import { JwtService } from "@nestjs/jwt";
+import jwtConfig from "../config/jwt.config";
+import { ConfigType } from "@nestjs/config";
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
     private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
   async signUp(signUpDto: SignUpDto) {
