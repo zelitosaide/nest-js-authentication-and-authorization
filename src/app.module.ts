e7992bbf462c5cb0fd6eb81e4c2c5b16ade09205
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 18:34:47 +0200

    NestJS Auth and Authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CoffeesModule } from "./coffees/coffees.module";
 import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [CoffeesModule, UsersModule],
+  imports: [
+    CoffeesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
