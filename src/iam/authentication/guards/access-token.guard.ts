f03f196527fcf997181ff30446b2f22d9c989b70
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 21:46:07 +0200

    NestJS Auth and Authorization

--- src/iam/authentication/guards/access-token.guard.ts
+++ src/iam/authentication/guards/access-token.guard.ts
@@ -1,11 +1,31 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from "@nestjs/common";
+import { ConfigType } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
 import { Observable } from "rxjs";
+import jwtConfig from "src/iam/config/jwt.config";
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [_, token] = request.headers.authorization?.split(" ") ?? [];
+    return token;
   }
 }
