68c893a966a9ad474a68e77620d7420f01396c1a
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 19:29:52 +0200

    NestJS Auth and Authorization

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/users/entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+}
