0f9b6690c4d6b145a23de115ea6c3b8143dd0cfc
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 19:11:07 +0200

    NestJS Auth and Authorization

--- src/iam/bcrypt.service.ts
+++ src/iam/bcrypt.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { HashingService } from "./hashing.service";
+import { compare, genSalt, hash } from "bcrypt";
 
 @Injectable()
 export class BcryptService implements HashingService {
-  hash(data: string | Buffer): Promise<string> {
-    throw new Error();
+  async hash(data: string | Buffer): Promise<string> {
+    const salt = await genSalt();
+    return hash(data, salt);
   }
 
   compare(data: string | Buffer, encrypted: string): Promise<boolean> {
-    throw new Error();
+    return compare(data, encrypted);
   }
 }
