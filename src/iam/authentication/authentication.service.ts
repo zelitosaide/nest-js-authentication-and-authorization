121289d4c2d435a700cc65d4ec639ed79d90ada4
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 19:38:00 +0200

    NestJS Auth and Authorization

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -2,10 +2,21 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
+import { HashingService } from "../hashing/hashing.service";
+import { SignUpDto } from "./dto/sign-up.dto";
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
   ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    const user = new User();
+    user.email = signUpDto.email;
+    user.password = await this.hashingService.hash(signUpDto.password);
+
+    await this.usersRepository.save(user);
+  }
 }
