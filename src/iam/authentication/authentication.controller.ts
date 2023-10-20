37a8dce2f82f4e6c8466ee153634b73faee94e6c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 19:19:13 +0200

    NestJS Auth and Authorization

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from "@nestjs/common";
 
-@Controller('authentication')
+@Controller("authentication")
 export class AuthenticationController {}
--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 import { HashingService } from "./hashing/hashing.service";
 import { BcryptService } from "./hashing/bcrypt.service";
-import { AuthenticationController } from './authentication/authentication.controller';
-import { AuthenticationService } from './authentication/authentication.service';
+import { AuthenticationController } from "./authentication/authentication.controller";
+import { AuthenticationService } from "./authentication/authentication.service";
 
 @Module({
   // providers: [HashingService, BcryptService],
