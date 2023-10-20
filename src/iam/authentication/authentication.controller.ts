affb39e0a8d8b17c9e4dcf6adbbd919afc436878
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-20 21:37:09 +0200

    NestJS Auth and Authorization

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from "@nestjs/common";
 import { AuthenticationService } from "./authentication.service";
 import { SignUpDto } from "./dto/sign-up.dto";
 import { SignInDto } from "./dto/sign-in.dto";
+import { Response } from "express";
 
 @Controller("authentication")
 export class AuthenticationController {
@@ -14,7 +22,17 @@ export class AuthenticationController {
 
   @HttpCode(HttpStatus.OK)
   @Post("sign-in")
-  signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto);
+  // signIn(@Body() signInDto: SignInDto) {
+  async signIn(
+    @Res({ passthrough: true }) response: Response,
+    @Body() signInDto: SignInDto,
+  ) {
+    // return this.authService.signIn(signInDto);
+    const accessToken = await this.authService.signIn(signInDto);
+    response.cookie("accessToken", accessToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: true,
+    });
   }
 }
