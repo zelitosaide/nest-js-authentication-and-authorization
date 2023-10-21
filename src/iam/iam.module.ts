05c089737979c320adc7705a001741a2408fca63
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-21 20:38:14 +0200

    NestJS Auth and Authorization

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -13,6 +13,7 @@ import { AccessTokenGuard } from "./authentication/guards/access-token.guard";
 import { AuthenticationGuard } from "./authentication/guards/authentication.guard";
 import { RefreshTokenIdsStorage } from "./authentication/refresh-token-ids.storage";
 import { RolesGuard } from "./authorization/guards/roles.guard";
+import { PermissionsGuard } from "./authorization/guards/permissions.guard";
 
 @Module({
   imports: [
@@ -33,7 +34,8 @@ import { RolesGuard } from "./authorization/guards/roles.guard";
     },
     {
       provide: APP_GUARD,
-      useClass: RolesGuard,
+      // useClass: RolesGuard,
+      useClass: PermissionsGuard,
     },
     AccessTokenGuard,
     RefreshTokenIdsStorage,
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { Role } from "../enums/role.enum";
+import { Permission, PermissionType } from "src/iam/authorization/permission.type";
 
 @Entity()
 export class User {
@@ -14,4 +15,11 @@ export class User {
 
   @Column({ enum: Role, default: Role.Regular })
   role: Role;
+
+  // 📝 ADDITIONS - user.entity.ts - add permissions prop
+  // NOTE: Having the "permissions" column in combination with the "role"
+  // likely does not make sense. We use both in this course just to showcase
+  // two different approaches to authorization.
+  @Column({ enum: Permission, default: [], type: 'json' })
+  permissions: PermissionType[];
 }
