f1c8006b081a4b4188a2fb30cb0c9286d2bad9ee
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-21 20:45:04 +0200

    NestJS Auth and Authorization

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -94,7 +94,12 @@ export class AuthenticationService {
       this.signToken<Partial<ActiveUserData>>(
         user.id,
         this.jwtConfiguration.accessTokenTtl,
-        { email: user.email, role: user.role, permissions: user.permissions },
+        {
+          email: user.email,
+          role: user.role,
+          // ⚠️ WARNING
+          permissions: user.permissions,
+        },
       ),
       this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl, {
         refreshTokenId,
