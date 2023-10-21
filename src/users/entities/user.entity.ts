7c377dbc96f5911d61b8865fe819074f4ec324f7
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-21 20:41:24 +0200

    NestJS Auth and Authorization

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,9 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { Role } from "../enums/role.enum";
-import { Permission, PermissionType } from "src/iam/authorization/permission.type";
+import {
+  Permission,
+  PermissionType,
+} from "src/iam/authorization/permission.type";
 
 @Entity()
 export class User {
@@ -20,6 +23,6 @@ export class User {
   // NOTE: Having the "permissions" column in combination with the "role"
   // likely does not make sense. We use both in this course just to showcase
   // two different approaches to authorization.
-  @Column({ enum: Permission, default: [], type: 'json' })
+  @Column({ enum: Permission, default: [], type: "json" })
   permissions: PermissionType[];
 }
